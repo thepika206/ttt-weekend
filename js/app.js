8403f0a4e2975323d54d5b424518053ef63282c4
pcheung <pika206@gmail.com>
2022-09-01 16:42:47 -0500

    add cached el ref for board and eventlistener

--- js/app.js
+++ js/app.js
@@ -4,15 +4,19 @@ console.log('san check')
 
 
 /*---------------------------- Variables (state) ----------------------------*/
-
+let board //to be an array representing the state of the 9 spaces on the play area
+let turn //state for who's turn it is
+let winner //is there a winner or if a time 
 
 
 /*------------------------ Cached Element References ------------------------*/
 
-
+const squareEls = document.querySelector("section.board")
 
 /*----------------------------- Event Listeners -----------------------------*/
-
+squareEls.addEventListener('click',function(evt){
+  console.log(evt.target.id)
+})
 
 
 /*-------------------------------- Functions --------------------------------*/
