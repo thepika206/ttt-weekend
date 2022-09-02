4bed2481a0aa1db1ced5513bc9edc639bcdd71c0
pcheung <pika206@gmail.com>
2022-09-02 09:13:36 -0500

    add reset game function

--- js/app.js
+++ js/app.js
@@ -16,19 +16,19 @@ const winningCombos = [
 let board //to be an array representing the state of the 9 spaces on the play area
 let turn //!Player O is -1, Player X is 1
 let winner //determines winner state null:no_winner, 1:Player X, -1:Player O, 'T':tie
-let moves //*counts moves taken, ties occurs on move 9 if no winner
 
 /*------------------------ Cached Element References ------------------------*/
 
 const squareEls = document.querySelector('section.board')
 const messageEl = document.querySelector('#message')
+const resetBtn = document.querySelector('#reset')
 
 /*----------------------------- Event Listeners -----------------------------*/
 //each div in section.board has an id, so evt.target.id will be used to set state of board
 squareEls.addEventListener('click',function(evt){
   if (!winner && evt.target.id) handleClick(evt)
 })
-
+resetBtn.addEventListener('click', init)
 
 /*-------------------------------- Functions --------------------------------*/
 init()
@@ -45,7 +45,7 @@ function init () {
     null,
     null,
   ]
-  moves = 0
+  // board = [1, 1, -1, -1, -1, 1, 1, -1, null] //* uncomment this line for a quick cats game test
   turn = 1
   winner = null
   console.log(`initial board:`, board, 'turn:', turn, 'winner:', winner)
@@ -54,12 +54,14 @@ function init () {
 
 function render(){
   board.forEach(function(element, idx){
+    const squareEl = document.getElementById(`sq${idx}`)
     if (element !== null) {
-      const squareEl = document.getElementById(`sq${idx}`)
-      squareEl.textContent = element === 1 ? "X" : "O"
-    }
+      squareEl.textContent = element === 1 ? 'X' : 'O'
+    } else {
+      squareEl.textContent = ''
+    } 
   })
-  let message 
+  let message =''
   if (winner === 'T') {
     message = `No empty spaces remain, looks like a tie game`
   } else if (winner === null) {
@@ -79,22 +81,24 @@ function handleClick(evt){
   }else{
     board[sqIdx] = turn
     turn = turn * -1
-    moves += 1
   }
   getWinner()
-  console.log('board:', board, 'moves:',moves, 'turn:', turn, 'winner:', winner)
+  console.log('board:', board, 'turn:', turn, 'winner:', winner)
   render()
 }
 
 function getWinner(){
-  //add up the values of the board at particular indices per each of the winningCombos trios
-  //if the sum equals -3 means player O won, 3 means player X won
+  //declare a winner by adding up the board postion values at the winning combos
+  //-3 means player O won, 3 means player X won
   for (let combo of winningCombos){
-    let result = board[combo[0]]+board[combo[1]]+board[combo[2]]
-    result === -3 ? winner = -1 : result
-    result === 3 ? winner = 1 : result  
+    let total = board[combo[0]]+board[combo[1]]+board[combo[2]]
+    total === -3 ? winner = -1 : total
+    total === 3 ? winner = 1 : total  
   }
-  winner = (moves === 9 && winner === null) ? 'T' : winner
+  // declare tie if any space is (!null)
+  winner = board.some(function(sq){return sq === null}) ? winner : 'T'
 }
 
 console.log('sanity check')
+
+
